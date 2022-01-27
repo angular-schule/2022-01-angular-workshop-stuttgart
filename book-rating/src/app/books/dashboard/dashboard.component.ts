958680f1a162fdf7846b98fb623a070bcf4d3d9f
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-27 12:16:48 +0100

    Events

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -33,4 +33,12 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  doRateUp(book: Book) {
+    console.log('UP', book);
+  }
+
+  doRateDown(book: Book) {
+    console.log('DOWN', book);
+  }
+
 }
