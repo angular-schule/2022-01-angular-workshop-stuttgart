ae99708d9fafbf08d2c27755de7c1b108e2d7986
Ferdinand Malcher <ferdinand@malcher.media>
2022-02-01 15:15:24 +0100

    Fun with Observables

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -22,7 +22,58 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+
+    // of('A', 'B', 'C')
+    // from([1,2,3,4,5])
+    // interval(1000)
+    // timer(3000)
+
+    timer(3000, 500).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+
+
+    /*
+    const myOf$ = new Observable((observer) => {
+      observer.next('A');
+      observer.next('B');
+      observer.next('C');
+      observer.complete();
+    });*/
+
+
+    /******************************/
+
+    function producer(o: any) {
+      const result = Math.random();
+      o.next(result);
+      o.next(5);
+      o.next(7);
+
+      setTimeout(() => o.next(9), 2000);
+      setTimeout(() => o.next(10), 4000);
+      setTimeout(() => o.complete(), 6000);
+    }
+
+    const observer = {
+      next: (e: number) => console.log(e),
+      error: (e: string) => console.error(e),
+      complete: () => console.log('C')
+    };
+
+    // producer(observer);
+
+    const myObservable$ = new Observable<number>(producer); // Finnische Notation
+    // myObservable$.subscribe(observer);
+    // myObservable$.subscribe(e => console.log(e));
+
+
     /******************************/
   }
 
